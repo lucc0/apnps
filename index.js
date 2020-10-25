d5b4f8100a8b1c076fd7a9aac2e68c36032fb248
lucc0 <rlucca447@gmail.com>
2020-10-24 21:43:21 -0300

    arquivo index.js

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const app = express();
+
+
+app.get("/",function(req,res){
+
+
+    res.send("Olá Lucca!")
+
+
+});
+
+
+app.listen(3000,function(){
+
+    console.log("Conexão inicializada na porta 3000")
+
+
+});
